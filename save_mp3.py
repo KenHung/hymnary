5cc28cb00132151bb9b15ff180ffcd2dff1e6f0b
Ken Hung <eiekenhung@gmail.com>
2023-05-26 16:54:09 +0800

    post processing

--- save_mp3.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-
-# Load the processed audio file using pydub
-audio = AudioSegment.from_wav("cut_audio_file.wav")
-
-# Export the audio file as an MP3 file
-audio.export("cut_audio_file.mp3", format="mp3")
