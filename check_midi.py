271ca3845a403e65b5fd333400c33d51629894c6
Ken Hung <eiekenhung@gmail.com>
2023-05-26 09:46:34 +0800

    consolidate midi files

--- check_midi.py
+++
@@ -1,8 +0,0 @@
-import os
-
-expected_file_number = 716
-
-for i in range(1, expected_file_number + 1):
-    filename = "pianoteq_output/hymnary{:03d}.wav".format(i)
-    if not os.path.exists(filename):
-        print(f"File {filename} is missing!")
